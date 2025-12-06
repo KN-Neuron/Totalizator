ef6cb3a6908294799d74f4a47f22389ec6a2352d
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2025-12-06 21:35:42 +0100

    adnimations: g

--- src/Play.js
+++ src/Play.js
@@ -1,39 +1,47 @@
 import Phaser from 'phaser';
 import { cardGridManager } from './cards/cardGridManager.js';
 
-export class Play extends Phaser.Scene
-{
+export class Play extends Phaser.Scene {
     cardGrid = null;
 
-    constructor ()
-    {
+    constructor() {
         super({
             key: 'Play'
         });
     }
+
     init() {
         // Fadein camera
         this.cameras.main.fadeIn(500);
+        // this.lives = 10;
+        // this.volumeButton();
     }
+
     create() {
         // Background image
         this.add.image(0, 0, "background").setOrigin(0);
+
         const titleText = this.add.text(this.sys.game.scale.width / 2, this.sys.game.scale.height / 2,
             "Gra totalizatora sportowego\nNacisnij aby zaczac",
             { align: "center", strokeThickness: 4, fontSize: 40, fontStyle: "bold", color: "#8c7ae6" }
         )
             .setOrigin(.5)
+
             .setDepth(3)
             .setInteractive();
+
         // title tween like retro arcade
         this.add.tween({
+
             targets: titleText,
             duration: 800,
             ease: (value) => (value > .8),
             alpha: 0,
+
             repeat: -1,
             yoyo: true,
         });
+
         // Text Events
         titleText.on(Phaser.Input.Events.POINTER_OVER, () => {
             titleText.setColor("#9c88ff");
@@ -57,46 +65,69 @@ export class Play extends Phaser.Scene
                 }
             })
         });
+
     }
+
     restartGame() {
         this.cameras.main.fadeOut(200 * this.cards.length);
     }
+
+    createInitialCards() {
+        const suits = ['diamondsAce', 'heartsAce', 'clubsAce', 'spadesAce'];
+
+        for (let row = 0; row < 4; row++) {
+            const cardData = {
+                type: suits[row],
+                onClick: (card) => {
+                    this.cardGrid.moveCard(row, true);
+                }
+            }
+
+            this.cardGrid.createCard(row, 0, cardData);
+        }
+
+        for (let col = 1; col < 7; col++) {
+            const cardData = {
+                type: 'back',
+                onClick: (card) => {
+                    // Row 4 cards - could add different behavior if needed
+                }
+            }
+
+            this.cardGrid.createCard(4, col, cardData);
+        }
+    }
+
     startGame() {
-        const frames = this.textures.get('cards').getFrameNames();
-        const cards = [];
-        cards.push(this.add.sprite(0, 0, 'cards', 'diamondsAce').setScale(0.7));
-        for (var a = 0; a < 5; a++) {
-            cards.push(this.add.sprite(0, 0))
-        }
-        cards.push(this.add.sprite(0, 0, 'cards', 'clubsAce').setScale(0.7));
-        for (var a = 0; a < 5; a++) {
-            cards.push(this.add.sprite(0, 0))
-        }
-        cards.push(this.add.sprite(0, 0, 'cards', 'heartsAce').setScale(0.7));
-        for (var a = 0; a < 5; a++) {
-            cards.push(this.add.sprite(0, 0))
-        }
-        cards.push(this.add.sprite(0, 0, 'cards', 'spadesAce').setScale(0.7));
-        for (var a = 0; a < 5; a++) {
-            cards.push(this.add.sprite(0, 0))
-        }
-        cards.push(this.add.sprite(0, 0))
-        for (var a = 0; a < 5; a++) {
-            cards.push(this.add.sprite(0, 0, 'cards', 'back').setScale(0.7));
-        }
-        Phaser.Actions.GridAlign(cards, {
-            width: 6,
-            height: 5,
-            cellWidth: 180,
-            cellHeight: 145,
-            x: 100,
-            y: 100
+
+        this.cardGrid = new cardGridManager(this, {
+            rows: 5,
+            columns: 7,
+            cardWidth: 150,
+            cardHeight: 100,
+            marginX: 20,
+            marginY: 30,
+            visibleColumns: 7
         });
-        // Balatro-style breathing animation with slight rotation
+
+        this.cardGrid.createGrid();
+
+        this.createInitialCards();
+
+        // Get all created cards from the grid
+        const cards = [];
+        for (let row = 0; row < this.cardGrid.config.rows; row++) {
+            for (let col = 0; col < this.cardGrid.config.columns; col++) {
+                const card = this.cardGrid.getCardAt(row, col);
+                if (card) {
+                    cards.push(card);
+                }
+            }
+        }
+
         cards.forEach((card, index) => {
             if (card.texture && card.texture.key === 'cards') {
                 const delay = index * 50;
-                // Add some randomness to make it more organic
                 const rotationVariation = Phaser.Math.Between(3500, 4500);
 
                 // Scale breathing
@@ -125,7 +156,7 @@ export class Play extends Phaser.Scene
                 // Smooth rotation wobble - much longer duration for fluid motion
                 this.tweens.add({
                     targets: card,
-                    angle: 2,
+                    angle: 94,
                     duration: rotationVariation,
                     delay: delay + 100,
                     yoyo: true,
@@ -135,3 +166,4 @@ export class Play extends Phaser.Scene
             }
         });
     }
+}
